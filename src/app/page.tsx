cfdaee567c676ba3a5368406d7ebf62e4ae44d71
mkit2009 <matejkotrba.skola@gmail.com>
2023-05-27 23:11:10 +0200

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { Fragment, Suspense } from "react";
-import Navbar from "~/components/Navbar";
-
 export default function Home() {
   return (
-    <Fragment>
-      <Suspense fallback={<p>Loading ...</p>}>
-        {/* @ts-ignore */}
-        <Navbar />
-      </Suspense>
-      <p>asoduhasiugd</p>
-    </Fragment>
+    <>
+      {/* <h2>Welcome to Countdown</h2> */}
+      <div>
+        <p>
+          <span className="text-md">How many days until the </span>
+          <input
+            type="text"
+            className="overflow-hidden text-3xl duration-200 bg-transparent border-b-2 outline-none border-slate-400 focus-within:border-blue-500 overflow-ellipsis"
+            style={{ width: "fit-content" }}
+          />
+        </p>
+      </div>
+    </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,7 @@
-export default async function Navbar() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const result: any[] = await new Promise(async (resolve: any) => {
-    const a = await data.json();
-    setTimeout(() => {
-      resolve(a);
-    }, 2000);
-  });
-
+export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-      </ul>
-      {result.map((item: any) => {
-        return (
-          <ul key={item.id}>
-            <li>
-              <a href={`/post/${item.id}`}>{item.title}</a>
-            </li>
-          </ul>
-        );
-      })}
+    <nav className="p-2 bg-gray-800">
+      <h3>Countdown</h3>
     </nav>
   );
 }
