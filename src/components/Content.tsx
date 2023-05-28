b4623eaadcd87861edb70934a1f2add17cf913e2
mkit2009 <matejkotrba.skola@gmail.com>
2023-05-28 19:18:16 +0200

    Displaying days until event

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -8,17 +8,42 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function Content() {
   const [startDate, setStartDate] = useState<Date | null>(null);
 
+  const dateDifference = useMemo(() => {
+    if (!startDate) return;
+
+    const today = new Date();
+
+    let differenceInDays =
+      (startDate?.getTime()! - today.getTime()) / 1000 / 60 / 60 / 24;
+
+    let resultContent = [];
+
+    for (let i = 0; i < differenceInDays; i++) {
+      resultContent.push(
+        <div
+          key={i}
+          className="text-5xl text-center text-white bg-transparent border-b-2 border-white border-solid outline-none"
+        >
+          {i}
+        </div>
+      );
+    }
+
+    return resultContent;
+  }, [startDate]);
+
   return (
     <div className="flex flex-col items-center gap-4">
-      <p className="text-xl">When will it happen?</p>
+      <p className="text-xl">It will happen on</p>
       <DatePicker
         dateFormat={"dd/MM/yyyy"}
         minDate={new Date(new Date().getTime() + 24 * 60 * 60 * 1000)}
         selected={startDate}
         onChange={(date) => setStartDate(date)}
-        className="text-xl text-center text-white bg-transparent border-b-2 border-white border-solid outline-none"
+        className="py-2 text-xl text-center text-white bg-transparent border-b-2 border-white border-solid outline-none"
         placeholderText="Click to select a date"
       />
+      <div>{dateDifference}</div>
     </div>
   );
 }
